760251c48fafccceb29a626c2e0dc11bd204ccec
MrDecim <146644605+callmedeci@users.noreply.github.com>
2024-11-14 11:13:21 -0800

    First set up

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <section className="bg-zinc-800 w-full h-screen flex flex-col">
+      <header className="flex flex-col">
+        <pre className="text-blue-500 text-clip text-sm md:text-xl relative"></pre>
+      </header>
+    </section>
+  );
+}
