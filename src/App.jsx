3c2077d8ee48c0d22ad20466c4103cc6ed693892
MrDecim <146644605+callmedeci@users.noreply.github.com>
2024-11-15 07:40:29 -0800

    Fix positioning

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const linksData = [
 
 export default function App() {
   return (
-    <section className="bg-ctp-mantle text-ctp-text w-full min-h-dvh">
+    <section className="bg-ctp-mantle text-ctp-text w-full min-h-dvh flex items-center justify-center">
       <div className="w-full h-full flex flex-col items-center justify-center max-w-sm md:max-w-2xl px-5 justify-self-center gap-6">
         <Header />
         <LinksList links={linksData} />
